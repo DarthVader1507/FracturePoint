18c85589f9943b41ee0a27cf88b5b7582ecb4655
DarthVader1507 <ashwath.chandramouli06@gmail.com>
2025-06-30 17:16:24 +0530

    DoorTrap added

--- My project/Assets/Scripts/Door.cs
+++ My project/Assets/Scripts/Door.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Door : MonoBehaviour
 {
-    [SerializeField]private Transform previous_room;
-    [SerializeField]private Transform next_room;
-    [SerializeField]private CameraControler cam;
+    [SerializeField] private Transform previous_room;
+    [SerializeField] private Transform next_room;
+    [SerializeField] private CameraControler cam;
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player"){
+        if (collision.CompareTag("Player")) {
             previous_room.gameObject.SetActive(false);
             next_room.gameObject.SetActive(true);
         }
+    }
+
+    // Call this to activate the trap (block the door)
+    public void ActivateTrap()
+{
+    if (boxCollider != null)
+    {
+        boxCollider.isTrigger = false; // Door blocks player
+        Debug.Log($" trap activated: isTrigger = {boxCollider.isTrigger}");
+    }
+}
+
+    // Call this to deactivate the trap (allow passage)
+    public void DeactivateTrap()
+    {
+        if (boxCollider != null)
+            boxCollider.isTrigger = true; // Door lets player through
     }
 }
--- My project/Assets/Scripts/TimerScript.cs
+++ My project/Assets/Scripts/TimerScript.cs
@@ -1,26 +1,34 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 public class TimerScript : MonoBehaviour
 {
-    [SerializeField]private Text timerText;
+    [SerializeField] private Text timerText;
     private float timeLeft;
     private CountdownScript countdownScript;
     [SerializeField] private GameObject Level;
+    [SerializeField] private SpriteRenderer doorSpriteRenderer1;
+    [SerializeField] private SpriteRenderer doorSpriteRenderer2;
+    [SerializeField] private SpriteRenderer doorSpriteRenderer3;
+    [SerializeField] private SpriteRenderer doorSpriteRenderer4;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if(SceneManager.GetActiveScene().name == "Level1")
+        if (SceneManager.GetActiveScene().name == "Level1")
         {
             timeLeft = 20f; // Set the timer for Level 1
         }
-        else if(SceneManager.GetActiveScene().name == "Level2")
+        else if (SceneManager.GetActiveScene().name == "Level2")
         {
             timeLeft = 40f; // Set the timer for Level 2
         }
-        else if(SceneManager.GetActiveScene().name == "Level3")
+        else if (SceneManager.GetActiveScene().name == "Level3")
         {
             timeLeft = 60f; // Set the timer for Level 3
+            SetDoorSprites(false); // Disable the door sprites at the start of Level 3
+            StartCoroutine(DoorTrap()); // Start the repeating trap
+            Debug.Log("Starting DoorTrap coroutine for Level 3");
         }
         countdownScript = FindObjectOfType<CountdownScript>();
         timerText.text = timeLeft.ToString("F2");
@@ -50,6 +58,50 @@ public class TimerScript : MonoBehaviour
             timeLeft = 0;
             timerText.text = "Time's up!";
             Level.SetActive(false); // Deactivate the level when time is up
+        }
+    }
+    private IEnumerator DoorTrap()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f); // Wait for 1 second before activating the door trap
+            SetDoorSprites(true); // Enable the door sprites when the trap is activated
+            SetDoorTraps(true); // Enable the door colliders to allow player interaction
+            yield return new WaitForSeconds(2f); // Wait for 1 second before deactivating the level
+            SetDoorSprites(false); // Disable the door sprites after the trap is activated
+            SetDoorTraps(false); // Disable the door colliders to prevent player interaction
+        }
+    }
+    private void SetDoorSprites(bool enabled)
+    {
+        doorSpriteRenderer1.enabled = enabled;
+        doorSpriteRenderer2.enabled = enabled;
+        doorSpriteRenderer3.enabled = enabled;
+        doorSpriteRenderer4.enabled = enabled;
+
+    }
+    // Add this method to your TimerScript class
+
+    private void SetDoorTraps(bool activate)
+    {
+        Debug.Log("SetDoorTraps called: " + activate);
+        var doors = new Door[]
+        {
+        doorSpriteRenderer1.gameObject.GetComponentInParent<Door>(),
+        doorSpriteRenderer2.gameObject.GetComponentInParent<Door>(),
+        doorSpriteRenderer3.gameObject.GetComponentInParent<Door>(),
+        doorSpriteRenderer4.gameObject.GetComponentInParent<Door>()
+        };
+
+        foreach (var door in doors)
+        {
+            if (door != null)
+            {
+                if (activate)
+                    door.ActivateTrap();
+                else
+                    door.DeactivateTrap();
+            }
         }
     }
 }
