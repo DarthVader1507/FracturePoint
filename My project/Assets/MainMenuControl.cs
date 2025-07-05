e7d6551366e3cca90a39341b68173b532e106fdc
DarthVader1507 <ashwath.chandramouli06@gmail.com>
2025-07-05 19:06:10 +0530

    Tutorial part 1 added

--- My project/Assets/MainMenuControl.cs
+++ My project/Assets/MainMenuControl.cs
@@ -4,6 +4,7 @@ public class MainMenuControl : MonoBehaviour
 {
     [SerializeField] private GameObject MainScreen;
     [SerializeField] private GameObject TutorialScreen;
+    [SerializeField] private GameObject ControlsScreen;
     public void Play()
     {
         Time.timeScale = 1;
@@ -17,10 +18,24 @@ public class MainMenuControl : MonoBehaviour
     {
         MainScreen.SetActive(false);
         TutorialScreen.SetActive(true);
+        ControlsScreen.SetActive(false);
     }
     public void HideTutorial()
     {
         MainScreen.SetActive(true);
         TutorialScreen.SetActive(false);
+        ControlsScreen.SetActive(false);
+    }
+    public void ShowControls()
+    {
+        MainScreen.SetActive(false);
+        TutorialScreen.SetActive(false);
+        ControlsScreen.SetActive(true);
+    }
+    public void HideControls()
+    {
+        MainScreen.SetActive(false);
+        TutorialScreen.SetActive(true);
+        ControlsScreen.SetActive(false);
     }
 }
